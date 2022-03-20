7bf09e220645818d9a3c4379a776960722e3656a
blankspeedster <ronie.bituin@trainocate.com>
2022-03-20 23:33:25 +0800

    :100: Reject order in pharmacist

--- pharmacy/orders.php
+++ pharmacy/orders.php
@@ -56,7 +56,8 @@ include("head.php");
                                             <div class="col-lg-12 card shadow mb-4">
                                                 <div>
                                                     <label class="h6 mt-3"><b>{{orders[0].firstname}} {{orders[0].lastname}}</b></label>
-                                                    <label class="mt-3" style="float: right">Total: <b>₱{{orders[0].total_amount}}</b></label>
+                                                    <label class="mt-3" style="float: right"><b>Total: </b>₱{{orders[0].total_amount}}</label>
+                                                    <label class="mt-3 mr-2" style="float: right !important;"><b>Order ID: </b> {{orders[0].transaction_id}}</label>
                                                 </div>
                                                 <span v-for="o in orders">
                                                     <div class="col-lg-12 card mb-2">
@@ -71,6 +72,16 @@ include("head.php");
                                                 <div class="mb-2">
                                                     <span v-if="orders[0].status === '0' " class="badge badge-warning m-1" style="float: left !important; color: black;">Status: Awaiting for your confirmation</span>
                                                     <span v-if="orders[0].status === '-1' " class="badge badge-success m-1" style="float: left !important;">Status: On the Way</span>
+
+                                                    <button @click="confirmOrder(orders[0].transaction_id)" class="btn btn-sm btn-success m-1" style="float: right;">Confirm Order</button>
+
+                                                    <button class="btn btn-sm btn-danger m-1" data-toggle="dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="float: right;">Cancel Order</button>
+                                                    <div class="dropdown-menu shadow-danger mb-1">
+                                                        <span class="dropdown-item">Confirm Cancellation? This cannot be undone.</span>
+                                                        <a class="dropdown-item text-success" href="#">Cancel</a>
+                                                        <a @click="cancelOrder(orders[0].transaction_id)" class="dropdown-item text-danger">Confirm Cancellation</a>
+                                                    </div>
+
                                                 </div>
                                             </div>
                                         </span>
@@ -114,8 +125,45 @@ include("head.php");
                                     </div>
                                 </div>
                             </div>
-
                         </div>
+
+                        <!-- Cancelled Orders -->
+                        <div class="row">
+                            <!-- Cancelled Orders -->
+                            <div class="col-lg-12 mb-4">
+                                <div class="card shadow mb-4">
+                                    <div class="card-header">
+                                        <h6 class="m-0 font-weight-bold text-primary">Cancelled Orders</h6>
+                                    </div>
+                                    <div class="card-body">
+                                        <span v-for="orders in cancelledOrders">
+                                            <div class="col-lg-12 card shadow mb-4">
+                                                <div>
+                                                <label class="h6 mt-3"><b>{{orders[0].firstname}} {{orders[0].lastname}}</b></label>
+                                                    <label class="mt-3" style="float: right">Total: <b>₱{{orders[0].total_amount}}</b></label>
+                                                    <label class="mt-3 mr-2" style="float: right !important;"><b>Order ID: </b> {{orders[0].transaction_id}}</label>
+                                                </div>
+                                                <span v-for="o in orders">
+                                                    <div class="col-lg-12 card mb-2">
+                                                        <div class="mt-1 mb-1">
+                                                            <img :src="'../assets/images/'+o.product_url" width="100px" />
+                                                            Name: <b>{{o.product_name}}</b>;
+                                                            Qty: <b>{{o.count}}</b>
+                                                            Subtotal: <b>₱{{o.subtotal}}</b>
+                                                        </div>
+                                                    </div>
+                                                </span>
+                                                <div class="mb-2">
+                                                    <span v-if="orders[0].status === '0' " class="badge badge-warning m-1" style="float: left !important; color: black;">Status: Awaiting for your confirmation</span>
+                                                    <span v-if="orders[0].status === '1' " class="badge badge-primary m-1" style="float: left !important;">Status: Completed</span>
+                                                </div>
+                                            </div>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                     <!-- /.container-fluid -->
 
@@ -154,6 +202,7 @@ include("head.php");
                             messageNotification: "",
                             currentOrders: [],
                             completedOrders: [],
+                            cancelledOrders: [],
                             user_id: <?php echo $_SESSION['user_id']; ?>,
                         }
                     },
@@ -204,10 +253,58 @@ include("head.php");
                                 });
                         },
 
+
+                        //Get Cancelled Orders
+                        async getCancelledOrders() {
+                            const options = {
+                                method: "POST",
+                                url: "process_order.php?getCancelledOrders",
+                                headers: {
+                                    Accept: "application/json",
+                                },
+                                data: {
+                                    user_id: this.user_id
+                                }
+                            };
+                            await axios
+                                .request(options)
+                                .then((response) => {
+                                    console.log(response);
+                                    this.cancelledOrders = response.data;
+                                })
+                                .catch((error) => {
+                                    console.log(error);
+                                });
+                        },                        
+
+                        //Get Completed Orders
+                        async cancelOrder(order_id) {
+                            const options = {
+                                method: "POST",
+                                url: "process_order.php?cancelOrder",
+                                headers: {
+                                    Accept: "application/json",
+                                },
+                                data: {
+                                    transaction_id: order_id
+                                }
+                            };
+                            await axios
+                                .request(options)
+                                .then((response) => {
+                                    console.log(response);
+                                    this.getCancelledOrders();
+                                })
+                                .catch((error) => {
+                                    console.log(error);
+                                });
+                        },
+
                     },
                     async mounted() {
                         this.getCurrentOrder();
                         this.getCompletedProducts();
+                        this.getCancelledOrders();
                     }
                 });
             </script>
--- pharmacy/process_order.php
+++ pharmacy/process_order.php
@@ -12,7 +12,7 @@ if (isset($_GET['getProducts'])) {
     $getCurrentOrders = mysqli_query($mysqli, "SELECT *, t.id AS transactionId, ps.id as userId
     FROM transaction t
     JOIN pharmacy_store ps ON ps.id = t.pharmacy_id
-    WHERE ps.user_id = '$user_id' AND t.status <> 1 ");
+    WHERE ps.user_id = '$user_id' AND t.status = '0' ");
 
     while ($order = mysqli_fetch_assoc($getCurrentOrders)) {
         $products = array();
@@ -72,6 +72,51 @@ if (isset($_GET['getCompletedProducts'])) {
     // }
 
     echo json_encode($orders);
+}
+
+//Get Cancelled Orders
+if (isset($_GET['getCancelledOrders'])) {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $user_id = $data["user_id"];
+    $orders = array();
+
+    $getCurrentOrders = mysqli_query($mysqli, "SELECT *, t.id AS transactionId FROM transaction t JOIN pharmacy_store ps ON ps.id = t.pharmacy_id WHERE t.status = '-2' ");
+
+    while ($order = mysqli_fetch_assoc($getCurrentOrders)) {
+        $products = array();
+        $transaction_id = $order["transactionId"];
+        $getProducts = mysqli_query($mysqli, " SELECT *, c.user_id as customer_id FROM cart c
+        JOIN pharmacy_products pp
+        ON pp.id = c.product_id
+        JOIN pharmacy_store ps
+        ON ps.id = c.pharmacy_id
+        JOIN transaction t
+        ON t.id = c.transaction_id
+        JOIN users u
+        ON u.id = t.user_id
+        WHERE c.transaction_id = '$transaction_id' ");
+
+        while ($product = mysqli_fetch_assoc($getProducts)) {
+            $products[] = $product;
+        }        
+        $orders[] = $products;
+    }
+
+    // while ($order = mysqli_fetch_assoc($getCurrentOrders)) {
+    //     $orders[] = $order;
+    // }
+
+    echo json_encode($orders);
+}
+
+//Cancel Order
+if (isset($_GET['cancelOrder'])) {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $transaction_id = $data["transaction_id"];
+
+    $getCurrentOrders = mysqli_query($mysqli, "UPDATE transaction SET status = '-1' WHERE id = '$transaction_id' ");
+    $response[] = array("response"=>"Order has been cancelled.");
+    echo json_encode($response);
 }
 
 ?>
