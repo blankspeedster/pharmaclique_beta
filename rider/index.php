54ad1e82742884ba5aae28cbdccd1fa817c9d9e3
Blank Speedster <ronie.bituin@trainocate.com>
2022-03-23 16:35:32 +0800

    Update index.php

--- rider/index.php
+++ rider/index.php
@@ -3,10 +3,10 @@ require_once("dbh.php");
 include("head.php");
 
 $user_id = $_SESSION['user_id'];
-$getCurrentBooking =  $mysqli->query("SELECT * FROM rider_transaction WHERE delivered = '0' ") or die($mysqli->error);
-if(mysqli_num_rows($getCurrentBooking) >= 1){
-    header("location: rides.php");
-}
+// $getCurrentBooking =  $mysqli->query("SELECT * FROM rider_transaction WHERE delivered = '0' ") or die($mysqli->error);
+// if(mysqli_num_rows($getCurrentBooking) >= 1){
+//     header("location: rides.php");
+// }
 ?>
 
 <title>PharmaClique - Rider Home</title>
